1a97da331a80167dfd38ba19d8f35cf77975aeca
wk96 <wk96.designs@gmail.com>
2015-12-04 19:52:25 +0300

    Template

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 WebGL js 3D shoter written via Threejs
 
 Testing first push
+
+---
+
+Root — Future Web-site with registration
+/game — The game directory
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title>The game</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
+	</head>
+	<body>
+		<div>Здесь будет авторизация и регистрация</div>
+		<div align="center"><a href="game/index.html">Игра</a></div>
+		<script src="js/three.min.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
